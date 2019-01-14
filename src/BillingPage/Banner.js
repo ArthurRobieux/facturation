49ea8f0e135ba5c2bec6a68fae716ddd89638d99
Arthur <arthur.robieux@gmail.com>
2019-01-14 18:09:52 +0100

    [3728] Improve style of CTA header

--- src/BillingPage/Banner.js
+++ src/BillingPage/Banner.js
@@ -5,12 +5,22 @@ class Banner extends Component {
   render() {
     return (
       <div id={"banner"}>
-          <div className={"banner_title"}>
-            Offrez-vous SportEasy Club
+
+          <div className={"banner_text"}>
+            <div className={"banner_title"}>
+              Offrez-vous SportEasy Club !
+            </div>
+
+            <div className={"banner_subtitle"}>
+              Plein d'avantage à prendre la version club.
+            </div>
+
           </div>
+
           <button id={"banner_button"} className={"common_button"}>
             Abonnez-vous à SportEasy Club
           </button>
+
       </div>
     );
   }
