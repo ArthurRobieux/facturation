5a75761cb2b21e4fb5f5d9535f1d73d06997141f
Arthur <arthur.robieux@gmail.com>
2019-01-16 12:25:03 +0100

    [3738] Add contacts list and possibility to add/remove contact

--- src/BillingPage/Contacts.js
+++ src/BillingPage/Contacts.js
@@ -5,10 +5,7 @@ class Contacts extends Component {
   constructor() {
     super();
     this.state = {
-      contact1: '',
-      contact2: '',
-      contact3: '',
-      contact4: '',
+      contactsList: ['arnaud@fortnite.fr', 'jpp@hotmail.fr'],
     };
     this.handleContact1Change = this.handleContact1Change.bind(this);
     this.handleContact2Change = this.handleContact2Change.bind(this);
@@ -18,16 +15,24 @@ class Contacts extends Component {
   }
 
   handleContact1Change(event) {
-    this.setState({contact1: event.target.value});
+    const contactsList = this.state.contactsList;
+    contactsList[0] = event.target.value;
+    this.setState({contactsList: contactsList});
   }
   handleContact2Change(event) {
-    this.setState({contact2: event.target.value});
+    const contactsList = this.state.contactsList;
+    contactsList[1] = event.target.value;
+    this.setState({contactsList: contactsList});
   }
   handleContact3Change(event) {
-    this.setState({contact3: event.target.value});
+    const contactsList = this.state.contactsList;
+    contactsList[2] = event.target.value;
+    this.setState({contactsList: contactsList});
   }
   handleContact4Change(event) {
-    this.setState({contact4: event.target.value});
+    const contactsList = this.state.contactsList;
+    contactsList[3] = event.target.value;
+    this.setState({contactsList: contactsList});
   }
 
   handleSubmit(event) {
@@ -35,7 +40,109 @@ class Contacts extends Component {
     event.preventDefault();
   }
 
+  showContact1(){
+    if(this.state.contactsList.length > 0) {
+      return (
+        <div className={"tab_text"}>
+          Contact 1
+          <div className={"form_element"}>
+            <div className={"form_text"}>
+              Adresse email
+            </div>
+            <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"}
+                   value={this.state.contactsList[0]} onChange={this.handleContact1Change}/>
+            <div className={"button_delete_contact"} onClick={() => this.deleteContact(0)}>X</div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  showContact2(){
+    if(this.state.contactsList.length > 1) {
+      return(
+        <div className={"tab_text"}>
+          Contact 2
+          <div className={"form_element"}>
+            <div className={"form_text"}>
+              Adresse email
+            </div>
+            <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"}
+                   value={this.state.contactsList[1]} onChange={this.handleContact2Change}/>
+            <div className={"button_delete_contact"} onClick={() => this.deleteContact(1)}>X</div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  showContact3(){
+    if(this.state.contactsList.length > 2) {
+      return (
+        <div className={"tab_text"}>
+          Contact 3
+          <div className={"form_element"}>
+            <div className={"form_text"}>
+              Adresse email
+            </div>
+            <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"}
+                   value={this.state.contactsList[2]} onChange={this.handleContact3Change}/>
+            <div className={"button_delete_contact"} onClick={() => this.deleteContact(2)}>X</div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  showContact4(){
+    if(this.state.contactsList.length > 3) {
+      return (
+        <div className={"tab_text"}>
+          Contact 4
+          <div className={"form_element"}>
+            <div className={"form_text"}>
+              Adresse email
+            </div>
+            <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"}
+                   value={this.state.contactsList[3]} onChange={this.handleContact4Change}/>
+            <div className={"button_delete_contact"} onClick={() => this.deleteContact(3)}>X</div>
+          </div>
+        </div>
+      )
+    }
+  }
+
+  addContactField(){
+    if(this.state.contactsList.length < 4){
+      let contactsList = this.state.contactsList;
+      contactsList.push("");
+      this.setState({contactsList: contactsList});
+    }
+  }
+
+  deleteContact(contact){
+    let contactsList = this.state.contactsList;
+    contactsList.splice(contact, 1);
+    this.setState({contactsList: contactsList});
+  }
+
+  showAddContactButton(){
+    try {
+      const addContactButton = document.getElementById("add_contact_button");
+      if (this.state.contactsList.length > 3) {
+        addContactButton.style.opacity = "0.25";
+        addContactButton.style.cursor = "default";
+      }
+      else {
+        addContactButton.style.opacity = "1";
+        addContactButton.style.cursor = "pointer";
+      }
+    }
+    catch{}
+  }
+
   render() {
+    this.showAddContactButton();
     return (
       <div className={"tab_container"}>
 
@@ -76,52 +183,19 @@ class Contacts extends Component {
 
             <form className={"tab_form"} onSubmit={this.handleSubmit}>
 
-              <div className={"tab_text"}>
-                Contact 1
-                <div className={"form_element"}>
-                  <div className={"form_text"}>
-                    Adresse email
-                  </div>
-                <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"} value={this.state.contact1} onChange={this.handleContact1Change}/>
-                </div>
-              </div>
-
-              <div className={"tab_text"}>
-                Contact 2
-                <div className={"form_element"}>
-                  <div className={"form_text"}>
-                    Adresse email
-                  </div>
-                <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"} value={this.state.contact2} onChange={this.handleContact2Change}/>
-                </div>
-              </div>
-
-              <div className={"tab_text"}>
-                Contact 3
-                <div className={"form_element"}>
-                  <div className={"form_text"}>
-                    Adresse email
-                  </div>
-                <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"} value={this.state.contact3} onChange={this.handleContact3Change}/>
-                </div>
-              </div>
-
-              <div className={"tab_text"}>
-                Contact 4
-                <div className={"form_element"}>
-                  <div className={"form_text"}>
-                    Adresse email
-                  </div>
-                <input className={"form_input"} type={"text"} placeholder={"arthur@sporteasy.net"} value={this.state.contact4} onChange={this.handleContact4Change}/>
-                </div>
-              </div>
-
-
+              {this.showContact1()}
+              {this.showContact2()}
+              {this.showContact3()}
+              {this.showContact4()}
 
               <div className={"form_element"}>
-                <button type={"submit"} className={"common_button"}>Sauvegarder</button>
+                <button type={"submit"} id={"add_contact_button"} className={"common_button"} onClick={() => this.addContactField()}>
+                  Ajouter un membre
+                </button>
+                <button type={"submit"} className={"common_button"}>
+                  Sauvegarder
+                </button>
               </div>
-
 
             </form>
 
